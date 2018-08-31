e239998795d71e06f401865b281a2f79a12284ee
benkrig <benjaminjkrig@gmail.com>
2018-08-31 16:09:56 -0700

    local db fix

--- api/utils/config.py
+++ api/utils/config.py
@@ -5,16 +5,16 @@ class Config(object):
 
 
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "sqlite://"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://"
 
 
 class DevelopmentConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/jsonx"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://"
     SQLALCHEMY_ECHO = False
 
 
 class TestingConfig(Config):
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@localhost/jsonx"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://"
     SQLALCHEMY_ECHO = False
