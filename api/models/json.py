55c229bc1a6cf895a487700354f6eb9d6808786f
benkrig <benjaminjkrig@gmail.com>
2018-09-27 13:43:16 -0700

    fix indentation and add to schema

--- api/models/json.py
+++ api/models/json.py
@@ -1,32 +1,34 @@
-from sqlalchemy import ForeignKeyConstraint
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from api.utils.database import db
 from marshmallow_sqlalchemy import ModelSchema
 from marshmallow import fields
-from api.utils.database import db
 
-db = SQLAlchemy(app)
 
 class Json(db.Model):
-	__tablename__='json_table'
+    __tablename__ = 'json'
 
-	id = db.Column(db.BigInteger, primary_key = True, autoincrement=True)
-	data = db.Column(db.String(10000))
-	created = db.Column(db.DateTime, server_default=db.func.now())
-	updated = db.Column(db.DateTime, onupdate=db.func.now())
+    id = db.Column(db.BigInteger, primary_key=True, autoincrement=True)
+    data = db.Column(db.String(10000))
+    created = db.Column(db.DateTime, server_default=db.func.now())
+    updated = db.Column(db.DateTime, onupdate=db.func.now())
 
-	def __init__(self,data):
-		self.data=data
+    def __init__(self, data):
+        self.data = data
 
-	def create(self):
-		db.session.add(self)
-		db.session.commit()
-		return self
+    def create(self):
+        db.session.add(self)
+        db.session.commit()
+        return self
+
 
 class JsonSchema(ModelSchema):
-	class Meta(ModelSchema.Meta):
-		model = Json
-		sqla_session = db.session
+    class Meta(ModelSchema.Meta):
+        model = Json
+        sqla_session = db.session
 
-	id = fields.Integer(dump_onyl=True)
-	data = fields.String(required=True)
-	created = fields.DateTime(dump_only=True)
-	updated = fields.DateTIme(dump_only=True)
+    id = fields.Integer(dump_onyl=True)
+    data = fields.String(required=True)
+    created = fields.String(dump_only=True)
+    updated = fields.String(dump_only=True)
